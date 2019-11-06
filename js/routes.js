1d24e0d61bad0c57c1146b3da036b08c1c658d24
Limor Ofri <limorofri7@gmail.com>
2019-11-06 19:49:59 +0200

    changed route miss keep

--- js/routes.js
+++ js/routes.js
@@ -1,6 +1,6 @@
 // import appHeader from './app-cmps/app-header.js';
 import missBooks from './apps/missBooks/cmps/book-app.cmp.js';
-import missKeep from './apps/missKeep/cmps/note-keep.cmp.js'
+import missKeep from './apps/missKeep/pages/note-keep-app.cmp.js'
 import missEmail from './apps/missEmail/cmps/email-app.cmp.js';
 
 
