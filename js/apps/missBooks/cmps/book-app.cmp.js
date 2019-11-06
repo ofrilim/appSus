cb52f63fb88924504a26881161f2d01cc26d2a21
Limor Ofri <limorofri7@gmail.com>
2019-11-06 14:54:57 +0200

    changes updated

---
+++ js/apps/missBooks/cmps/book-app.cmp.js
@@ -0,0 +1,31 @@
+
+
+
+export default {
+    name: '',
+    template: `
+        <section class="">
+            <h1>miss book app!</h1>
+        </section>
+    `,
+    data() {
+        return {
+
+        }
+    },
+    created() {
+         
+    },
+    methods: {
+       
+    },
+    computed: {
+
+    },
+    watch: {
+        
+    },
+    components: {
+        
+    }
+}
