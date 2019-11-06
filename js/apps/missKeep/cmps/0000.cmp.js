5c2f285976e2b3d44bb9969b86f19950a77aa39e
Nimrod Gaver <gavernimrod@gmail.com>
2019-11-06 13:50:17 +0200

    fixing

---
+++ js/apps/missKeep/cmps/0000.cmp.js
@@ -0,0 +1,31 @@
+
+
+
+export default {
+    name: '',
+    template: `
+        <section class="">
+            <h1>miss keep app!</h1>
+        </section>
+    `,
+    data() {
+        return {
+
+        }
+    },
+    created() {
+         
+    },
+    methods: {
+       
+    },
+    computed: {
+
+    },
+    watch: {
+        
+    },
+    components: {
+        
+    }
+}
